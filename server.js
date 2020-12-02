2bfa5d5aa776f67d17a849a6bf2eb033b4f2a1fc
fareska <fares.xb@gmail.com>
2020-12-02 14:34:26 +0200

    backend has been setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const api = require('./server/routes/api')
+const City = require('./server/model/City')
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/weatherDB', {useNewUrlParser: true})
+
+app.use('/', api)
+
+const port = 3200
+app.listen(port, ()=> {
+    console.log(`Server running on ${port}`)
+})
