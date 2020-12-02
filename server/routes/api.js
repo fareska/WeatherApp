0bca45af7bdcd4da351db90cb4731bb983c987b1
fareska <fares.xb@gmail.com>
2020-12-02 19:03:05 +0200

    the backend finished but not tested yet

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,9 +7,10 @@ const City = require('../model/City')
 
 const apiKey = "197cf43e7d01659ab1e8742f02fcaaa4"
 
+//This route should take a cityName parameter and return the city data in a response.
 router.get('/city/:cityName', function(req, res){
     const {cityName} = req.params
-
+    //axios.get didn't work for me ????????????????????????
     url.request(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
     function (err,cityData){
         const data = JSON.parse(cityData)
@@ -17,6 +18,23 @@ router.get('/city/:cityName', function(req, res){
     })
 })
 
+//This route should find all of the city data saved in your DB, and send it to the client
+router.get('/cities', function(req, res){
+    City.find({}, (err, data)=>{res.send(data)})
+})
+
+//This route should save a new City to your DB
+router.post('/city', (req, res)=> {
+    const newCity = new City (req.body)
+    newCity.save().then(newCity=> {res.send(newCity)}).catch(err=> res.send(err))
+})
+
+//This route should take a cityName parameter and delete the correct city from your DB
+router.delete('/city/:cityName', async (req, res)=> {
+    const { deleteCity } = req.params /// is the parameter a name because city name is unique?
+    const city = await City.findOneAndDelete(deleteCity)
+    res.send(city)
+})
 
 
 module.exports = router
